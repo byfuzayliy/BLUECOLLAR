2fb7dea3c38c5291f8affb491c3145122216cc61
Aziz Abduhakimov <byfuzayliy@gmail.com>
2025-05-12 19:10:22 +0500

    I have just added new section to this project

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,16 @@
-import React, { Component, Fragment } from "react";
+import { Component, Fragment } from "react";
 import { IoIosSettings } from "react-icons/io";
 import { RiUserSettingsLine } from "react-icons/ri";
+import { BsBuildingsFill, BsBuildingFillGear } from "react-icons/bs";
+import { RiBuilding2Line } from "react-icons/ri";
 
 import provide from "../../src/assets/images/provide.png";
 
+import electricalImage from "../assets/images/electrical.png";
+
 import "./home.css";
+
+import styles from "./commercial.module.css";
 export class Home extends Component {
   render() {
     return (
@@ -24,7 +30,6 @@ export class Home extends Component {
             </div>
           </div>
         </section>
-
         <section className="experience">
           <div className="container experience-wrapper">
             <div className="provide-img">
@@ -66,6 +71,76 @@ export class Home extends Component {
                   <p>- A Full Guarantee On Workmanship</p>
                   <p>- Properties at a reasonable price.</p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.commercial}>
+          <div className={`${styles.commercialWrapper} , container`}>
+            <span className={styles.offer}>
+              <RiUserSettingsLine />
+              What We Offer
+            </span>
+
+            <h3 className={styles.heading}>Professional Main Services</h3>
+            <p className={styles.infoOffer}>
+              We have extensive experience of domestic and business electrical
+              installations and no job is <br /> too small. Our customers value
+              our professionalism, work ethic and our competitive prices.
+            </p>
+
+            <div className={styles.merchOffer}>
+              <div className={styles.listOfOffer}>
+                <span>
+                  <BsBuildingsFill />
+                </span>
+                <h4>Commerical</h4>
+              </div>
+              <div className={styles.listOfOffer}>
+                <span>
+                  <BsBuildingFillGear />
+                </span>
+                <h4>Residential</h4>
+              </div>
+              <div className={styles.listOfOffer}>
+                <span>
+                  <RiBuilding2Line />
+                </span>
+                <h4>Indestrial</h4>
+              </div>
+            </div>
+
+            <div className={styles.electrical}>
+              <div className={styles.electricalInfo}>
+                <p>
+                  <span>
+                    <BsBuildingsFill />
+                  </span>
+                  Electrical installation
+                </p>
+
+                <h3>Commercial</h3>
+
+                <p className={styles.infoText}>
+                  Electrical & Maintenance Services Ltd offers a full range of
+                  electrical services to suit your needs – from moving a light
+                  switch to complete house rewires.
+                </p>
+
+                <br />
+
+                <p className={styles.infoText}>
+                  In addition to providing fully qualified, competent
+                  electricians in and around London, the company also runs an
+                  electrical wholesale shop.
+                </p>
+
+                <button className={styles.offerBtn}>Read More</button>
+              </div>
+
+              <div className={styles.electricalImage}>
+                <img src={electricalImage} alt="" />
               </div>
             </div>
           </div>
